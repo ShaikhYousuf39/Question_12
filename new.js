92334d427c4ef84679cbcb55a1266e14c26c6db1
YousufSH <160315514+ShaikhYousuf39@users.noreply.github.com>
2024-03-10 01:09:25 +0500

    Add files via upload

---
+++ new.js
@@ -0,0 +1,5 @@
+var names = ["Saad", "Ahmed", "Nafay"];
+var message = ("Do You Like To Play Free Fire With Me?");
+console.log(names[0] + " " + message);
+console.log(names[1] + " " + message);
+console.log(names[2] + " " + message);
---
+++ new.ts
@@ -0,0 +1,8 @@
+let names: string[] = ["Saad","Ahmed","Nafay"];
+
+let message: string = ("Do You Like To Play Free Fire With Me?");
+
+console.log(names[0] + " " + message);
+console.log(names[1] + " " + message);
+console.log(names[2] + " " + message);
+
